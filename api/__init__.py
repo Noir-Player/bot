12d05ccb5f43dac61992cfc4f68bfb76c1e2e789
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-01-28 19:51:34 +0300

    add custom class

--- api/__init__.py
+++ api/__init__.py
@@ -1,14 +1,44 @@
 from fastapi import FastAPI
 import api.routers as routers
 
-def __init__(bot) -> FastAPI:
+from uuid import UUID
+from fastapi_sessions.backends.implementations import InMemoryBackend
+
+from api.routers.helpers.session_verify import SessionData, BasicVerifier, HTTPException
+
+
+
+class NoirAPI(FastAPI):
+    def init_sessions(self):
+        self._sessions = InMemoryBackend[UUID, SessionData]()
+        self._verifier = BasicVerifier(
+            identifier="general_verifier",
+            auto_error=True,
+            backend=self._sessions,
+            auth_http_exception=HTTPException(status_code=403, detail="invalid session"),
+        )
+
+
+    @property
+    def sessions(self) -> InMemoryBackend[UUID, SessionData]:
+        return self._sessions
+    
+    @property
+    def verifier(self) -> BasicVerifier:
+        return self._verifier
+
+
+
+def __init__(bot) -> NoirAPI:
     """Return pathed FastAPI obj"""
-    app = FastAPI(
+    api = NoirAPI(
         title="Noir Player API",
         description="Noir Player API app, works on sessions without JWT",
         version="0.1.0"
     )
 
-    routers.include_modules(app, bot)
+    api.init_sessions()
 
-    return app
+    routers.include_modules(api, bot)
+
+    return api
