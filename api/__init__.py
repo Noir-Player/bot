df71ea2f876fc8dac081a94fd3270e1397a95de4
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-02-01 14:13:55 +0300

    remove sessions and add seckey

--- api/__init__.py
+++ api/__init__.py
@@ -1,43 +1,28 @@
 from fastapi import FastAPI
 import api.routers as routers
 
-from uuid import UUID
-from fastapi_sessions.backends.implementations import InMemoryBackend
+import uuid
 
-from api.routers.helpers.session_verify import SessionData, BasicVerifier, HTTPException
-
+from config import UUID_STRING
 
 
 class NoirAPI(FastAPI):
-    def init_sessions(self):
-        self._sessions = InMemoryBackend[UUID, SessionData]()
-        self._verifier = BasicVerifier(
-            identifier="general_verifier",
-            auto_error=True,
-            backend=self._sessions,
-            auth_http_exception=HTTPException(status_code=403, detail="invalid session"),
-        )
-
 
     @property
-    def sessions(self) -> InMemoryBackend[UUID, SessionData]:
-        return self._sessions
-    
-    @property
-    def verifier(self) -> BasicVerifier:
-        return self._verifier
+    def seckey(self) -> str:
+        return uuid.uuid5(uuid.NAMESPACE_X500, UUID_STRING).__str__()
 
 
 
 def __init__(bot) -> NoirAPI:
     """Return pathed FastAPI obj"""
     api = NoirAPI(
-        title="Noir Player API",
-        description="Noir Player API app, works on sessions without JWT",
-        version="0.1.0"
+        title = "Noir Player API",
+        description = "Noir Player API app. Simple, Graceful and Powerful discord player",
+        version = "0.2.0",
+        docs_url = None,
+        redoc_url = "/api-reference"
     )
-
-    api.init_sessions()
 
     routers.include_modules(api, bot)
 
