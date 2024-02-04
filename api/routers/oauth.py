67c818dcc73663a880dde2e69b787e3307f66561
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-02-04 13:27:45 +0300

    fix login

--- api/routers/oauth.py
+++ api/routers/oauth.py
@@ -30,8 +30,14 @@ def make_session(token=None, state=None, scope=None):
             'client_id': OAUTH2_CLIENT_ID,
             'client_secret': OAUTH2_CLIENT_SECRET,
         },
-        auto_refresh_url=TOKEN_URL,
+        # auto_refresh_url=TOKEN_URL,
         token_updater=None)
+
+def get_session_keys(cookies = None):
+
+    key = cookies.get("sessionid") or uuid.uuid4().__str__()
+
+    return uuid.uuid5(uuid.NAMESPACE_X500, f'{key}{router.salt}').__str__(), key
 
 # ---------------------------------------------------------------------------------------------------------
 
@@ -40,13 +46,12 @@ async def make_oauth(sessiontoken: str):
     discord = make_session(token=router.db.sessions.get_oauth_token(sessiontoken))
 
     # Set session
-    session = router.db.sessions.set(
-        {
+    session = {
             "user": discord.get("https://discordapp.com/api/users/@me").json(),
             "connections": discord.get('https://discordapp.com/api/users/@me/connections').json(),
-        },
-        token = sessiontoken
-    )
+        }
+
+    router.db.sessions.set(session, token = sessiontoken)
 
     # Make response with redirect (if exist)
     resp = RedirectResponse("/discovery")
@@ -83,66 +88,55 @@ async def make_oauth(sessiontoken: str):
     # Update user profile
     router.db.users.set_name(session["user"].get("username"),  int(session["user"].get("id")))
 
-    # Find user for theme
-
-    # # Try to find authtorized devices
-    # devices = router.db.cache.get(int(session['me'].get('id'))).get('devices')
-
-    # # If not found
-    # if not devices:
-    #     # Set one device: user_agent
-    #     router.db.cache.push({'devices': resp.user_agent.string},
-    #                   int(session['me'].get('id')))
-    # else:
-    #     if request.user_agent.string not in devices:
-    #         # If current device, skip
-    #         db.cache.push({'devices': request.user_agent.string},
-    #                       int(session['me'].get('id')))
-
     return resp
+    
 
 @router.get("/login")
-async def login(response: Response):
+async def login(request: Request):
 
-    key = uuid.uuid4()
-
-    sessid = uuid.uuid5(uuid.NAMESPACE_X500, f'{key}{router.salt}')
+    sessid, key = get_session_keys(request.cookies)
 
     lprint(f'{sessid} - {key}')
 
-    scope = 'identify connections guilds'
-
-    discord = make_session(scope=scope.split(' '))
+    discord = make_session(scope=SCOPES.split(' '))
 
     authorization_url, state = discord.authorization_url(AUTHORIZATION_BASE_URL)
-    router.db.sessions.set_oauth_state(sessid, SessionData(oauth2_state=state))
+    router.db.sessions.set_oauth_state(state, sessid or request.cookies.get("sessionid"))
 
-    response.set_cookie("sessionid", str(key), secure=True, httponly=True)
+    response = RedirectResponse(authorization_url)
 
-    return RedirectResponse(authorization_url)
+    response.set_cookie(key="sessionid", value=str(key))
+
+    return response
 
 
-@router.get('/callback', dependencies=[Depends(cookie)])
+@router.get('/callback')
 async def callback(request: Request, sessionid: str = Cookie()):
-
     if not sessionid:
-        return {"msg": "session not passed"}, 403
+        return {"msg": "session not passed"}
 
     discord = make_session(state=router.db.sessions.get_oauth_state(sessionid))
+
+    lprint(request.url)
 
     router.db.sessions.set_oauth_token(
         discord.fetch_token(
             TOKEN_URL,
             client_secret=OAUTH2_CLIENT_SECRET,
-            authorization_response=request.url
-        )
+            authorization_response=request.url.__str__().replace("http://", "https://")
+        ),
+        sessionid
     )
 
     return await make_oauth(sessionid)
 
-@router.get("/logout")
+@router.get("/logout", dependencies=[Depends(cookie)])
 async def logout(response: Response, sessionid: str = Cookie()):
+    if not sessionid:
+        return {"msg": "session not passed"}
+    
+    router.db.sessions.col().delete_many({})
     router.db.sessions.clear(sessionid)
-    cookie.delete_from_response(response)
+    response.delete_cookie("sessionid")
     return RedirectResponse("/")
 
