0ac104de9175ea4e66c5eb3d0549a68395ef4ce0
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-03-11 22:27:57 +0300

    add `AddResponse`

--- api/shemas/stars.py
+++ api/shemas/stars.py
@@ -1,11 +1,14 @@
 from pydantic import *
 from pydantic.fields import Field
-from typing import Optional
-from classes.WebModels import Stars, Track
+from typing import Optional, Union
+from classes.WebModels import Stars, Track, Playlist
 
 class Meta(BaseModel):
     page: int = Field(description="Текущая страница")
     count: int = Field(description="Количество отображений на странице")
+
+class MetaAdd(BaseModel):
+    index: int = Field(description="Позиция трека в звездочках")
     
 
 class StarsResponse(BaseModel):
@@ -14,3 +17,7 @@ class StarsResponse(BaseModel):
 
 class StarsTrackResponse(BaseModel):
     data: Optional[Track] = None
+
+class StarsTrackAddResponse(BaseModel):
+    data: Union[Playlist, Track]
+    meta: MetaAdd
