9075f3605e12890718a16246a1904f04a629c954
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-03-12 19:45:46 +0300

    add new session

--- api/routers/oauth.py
+++ api/routers/oauth.py
@@ -31,21 +31,13 @@ def make_session(token=None, state=None, scope=None):
         # auto_refresh_url=TOKEN_URL,
         token_updater=None)
 
-
-def get_session_keys(cookies=None):
-
-    key = cookies.get("sessionid") or uuid.uuid4().__str__()
-
-    return uuid.uuid5(uuid.NAMESPACE_X500,
-                      f'{key}{router.salt}').__str__(), key
-
 # ---------------------------------------------------------------------------------------------------------
 
 
 async def make_oauth(sessiontoken: str):
     # Make oauth
     discord = make_session(
-        token=router.db.sessions.get_oauth_token(sessiontoken))
+        token=router.session.get(sessiontoken).get("oauth_token"))
 
     # Set session
     session = {
@@ -53,7 +45,7 @@ async def make_oauth(sessiontoken: str):
         "connections": discord.get('https://discordapp.com/api/users/@me/connections').json(),
     }
 
-    router.db.sessions.set(session, token=sessiontoken)
+    router.session.set(sessiontoken, session)
 
     # Make response with redirect (if exist)
     resp = RedirectResponse("/discovery")
@@ -88,9 +80,7 @@ async def make_oauth(sessiontoken: str):
         {'guilds': {'same': same, 'other': other}}, session["user"].get("id"))
 
     # Update user profile
-    router.db.users.set_name(
-        session["user"].get("username"), int(
-            session["user"].get("id")))
+    router.db.users.add_user(session['user'])
 
     return resp
 
@@ -98,14 +88,15 @@ async def make_oauth(sessiontoken: str):
 @router.get("/login", description="Url для входа", status_code=307)
 async def login(request: Request):
 
-    sessid, key = get_session_keys(request.cookies)
-
     discord = make_session(scope=SCOPES.split(' '))
+
+    key = uuid.uuid4().__str__()
 
     authorization_url, state = discord.authorization_url(
         AUTHORIZATION_BASE_URL)
-    router.db.sessions.set_oauth_state(
-        state, sessid or request.cookies.get("sessionid"))
+    router.session.set(
+        request.cookies.get("sessionid") or key,
+        {"oauth_state": state}, 3600)
 
     response = RedirectResponse(authorization_url)
 
@@ -119,16 +110,16 @@ async def callback(request: Request, sessionid: str = Cookie()):
     if not sessionid:
         return {"msg": "session not passed"}
 
-    discord = make_session(state=router.db.sessions.get_oauth_state(sessionid))
+    discord = make_session(state=router.session.get(sessionid).get("oauth_state"))
 
-    router.db.sessions.set_oauth_token(
-        discord.fetch_token(
+    router.session.set(
+        sessionid,
+        {"oauth_token": discord.fetch_token(
             TOKEN_URL,
             client_secret=OAUTH2_CLIENT_SECRET,
             authorization_response=request.url.__str__().replace(
                 "http://",
-                "https://")),
-        sessionid)
+                "https://"))})
 
     return await make_oauth(sessionid)
 
@@ -138,7 +129,6 @@ async def logout(response: Response, sessionid: str = Cookie()):
     if not sessionid:
         return {"msg": "session not passed"}
 
-    router.db.sessions.col().delete_many({})
-    router.db.sessions.clear(sessionid)
+    router.session.pop(sessionid)
     response.delete_cookie("sessionid")
     return RedirectResponse("/")
