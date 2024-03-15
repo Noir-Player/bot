d73edfda3b966036c55738468d6623cdf3aaa3b2
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-03-15 15:48:08 +0300

    add search response

--- api/routers/discovery.py
+++ api/routers/discovery.py
@@ -1,7 +1,9 @@
 from classes.ApiRouter import NOIRouter
-from fastapi import Query, Cookie
+from fastapi import Query, Cookie, Response
 
 from api.shemas import *
+
+import pomice
 
 router = NOIRouter(prefix="/discovery", tags=["Discovery"])
 
@@ -33,6 +35,42 @@ async def get_discovery(
 async def search(
     sessionid: str = Cookie(),
     query: str = Query('', description="Запрос / ссылка"),
-    type: Optional[Literal["spsearch", "ytsearch", "ytmsearch", "scsearch", "dzsearch"]] = Query("spsearch", description="Тип поиска")
+    type: Literal["spsearch", "ytsearch", "ytmsearch", "scsearch", "dzsearch"] = Query("ytmsearch", description="Тип поиска")
 ):
-    return {}
+    await router.session.verify(sessionid)
+
+    only = tracks = playlists = mixes = None
+
+    node: pomice.Node = router.bot.node
+
+    user_playlists = router.db.playlists.find(query)
+
+    search_result = router.execute(node.get_tracks(query, search_type=pomice.SearchType(type)))
+
+    if not search_result:
+        return Response(content="Tracks, playlists and mixes not found", status_code=404)
+    
+    if hasattr(search_result, "tracks"):
+        playlists = [search_result.to_dict()]
+        only = "playlist"
+
+    else:
+        tracks = [track.to_dict() for track in search_result]
+
+        if len(search_result) == 1:
+            only = "track"
+
+
+    mixes = node.external.get_mixes("sp" if type == "spsearch" else "yt")
+
+    return {
+        "tracks": tracks,
+        "playlists": playlists,
+        "user_playlists": user_playlists,
+        "mixes": mixes,
+        "meta": {
+            "query": query,
+            "type": type,
+            "only": only
+        }
+    }
