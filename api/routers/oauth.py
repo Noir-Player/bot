c39d4169332b2d1d4b551387324201e0c1c04d9b
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-03-20 15:00:01 +0300

    review with black

--- api/routers/oauth.py
+++ api/routers/oauth.py
@@ -25,11 +25,13 @@ def make_session(token=None, state=None, scope=None):
         scope=scope,
         redirect_uri=OAUTH2_REDIRECT_URI,
         auto_refresh_kwargs={
-            'client_id': OAUTH2_CLIENT_ID,
-            'client_secret': OAUTH2_CLIENT_SECRET,
+            "client_id": OAUTH2_CLIENT_ID,
+            "client_secret": OAUTH2_CLIENT_SECRET,
         },
         # auto_refresh_url=TOKEN_URL,
-        token_updater=None)
+        token_updater=None,
+    )
+
 
 # ---------------------------------------------------------------------------------------------------------
 
@@ -37,12 +39,15 @@ def make_session(token=None, state=None, scope=None):
 async def make_oauth(sessiontoken: str):
     # Make oauth
     discord = make_session(
-        token=(await router.session.get(sessiontoken)).get("oauth_token"))
+        token=(await router.session.get(sessiontoken)).get("oauth_token")
+    )
 
     # Set session
     session = {
         "user": discord.get("https://discordapp.com/api/users/@me").json(),
-        "connections": discord.get('https://discordapp.com/api/users/@me/connections').json(),
+        "connections": discord.get(
+            "https://discordapp.com/api/users/@me/connections"
+        ).json(),
     }
 
     await router.session.set(sessiontoken, session)
@@ -51,23 +56,27 @@ async def make_oauth(sessiontoken: str):
     resp = RedirectResponse("/discovery")
 
     # Set servers
-    servers, same, other, mutual = discord.get('https://discordapp.com/api/users/@me/guilds').json(
-    ), [], [], [guild.id for guild in router.bot.guilds]
+    servers, same, other, mutual = (
+        discord.get("https://discordapp.com/api/users/@me/guilds").json(),
+        [],
+        [],
+        [guild.id for guild in router.bot.guilds],
+    )
 
     if isinstance(servers, dict):  # must be array
         return RedirectResponse("/login")
 
     for server in servers:
-        if server.get(
-                'permissions') & 8 == 8:  # Если есть разрешения администратора
-            server['settings'] = router.db.setup.get_setup(
-                int(session["user"].get("id")))
+        if server.get("permissions") & 8 == 8:  # Если есть разрешения администратора
+            server["settings"] = router.db.setup.get_setup(
+                int(session["user"].get("id"))
+            )
             try:
-                del server['settings']['_id']
+                del server["settings"]["_id"]
             except BaseException:
                 pass
 
-            if int(server.get('id')) in mutual:
+            if int(server.get("id")) in mutual:
                 same.append(server)
 
             else:
@@ -77,10 +86,11 @@ async def make_oauth(sessiontoken: str):
             pass
 
     router.db.cache.set(
-        {'guilds': {'same': same, 'other': other}}, session["user"].get("id"))
+        {"guilds": {"same": same, "other": other}}, session["user"].get("id")
+    )
 
     # Update user profile
-    router.db.users.add_user(session['user'])
+    router.db.users.add_user(session["user"])
 
     return resp
 
@@ -88,17 +98,12 @@ async def make_oauth(sessiontoken: str):
 @router.get("/login", description="Url для входа", status_code=307)
 async def login(request: Request):
 
-    discord = make_session(scope=SCOPES.split(' '))
+    discord = make_session(scope=SCOPES.split(" "))
 
     key = request.cookies.get("sessionid") or uuid.uuid4().__str__()
 
-    authorization_url, state = discord.authorization_url(
-        AUTHORIZATION_BASE_URL)
-    await router.session.set(
-        key,
-        {"oauth_state": state}, 
-        3600
-        )
+    authorization_url, state = discord.authorization_url(AUTHORIZATION_BASE_URL)
+    await router.session.set(key, {"oauth_state": state}, 3600)
 
     response = RedirectResponse(authorization_url)
 
@@ -107,21 +112,27 @@ async def login(request: Request):
     return response
 
 
-@router.get('/callback', description="Редирект после входа", status_code=307)
+@router.get("/callback", description="Редирект после входа", status_code=307)
 async def callback(request: Request, sessionid: str = Cookie()):
     if not sessionid:
         return {"msg": "session not passed"}
 
-    discord = make_session(state=(await router.session.get(sessionid)).get("oauth_state"))
+    discord = make_session(
+        state=(await router.session.get(sessionid)).get("oauth_state")
+    )
 
     await router.session.set(
         sessionid,
-        {"oauth_token": discord.fetch_token(
-            TOKEN_URL,
-            client_secret=OAUTH2_CLIENT_SECRET,
-            authorization_response=request.url.__str__().replace(
-                "http://",
-                "https://"))})
+        {
+            "oauth_token": discord.fetch_token(
+                TOKEN_URL,
+                client_secret=OAUTH2_CLIENT_SECRET,
+                authorization_response=request.url.__str__().replace(
+                    "http://", "https://"
+                ),
+            )
+        },
+    )
 
     return await make_oauth(sessionid)
 
@@ -137,6 +148,7 @@ async def logout(sessionid: str = Cookie()):
     response.delete_cookie("sessionid")
 
     return response
+
 
 # @router.get("/session", description="Получить свою сессию")
 # async def get_session(response: Response, sessionid: str = Cookie()):
