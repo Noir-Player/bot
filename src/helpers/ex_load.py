6c0a5654122f4eb94b3068d7782ab2b0e0d3fe73
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-05-22 21:34:30 +0300

    add exceptions

--- src/helpers/ex_load.py
+++ src/helpers/ex_load.py
@@ -2,47 +2,45 @@ from os import listdir
 
 
 def cogsReload(bot):
-    curr, total = 1, len(listdir("./cogs")) - 3  # cogs - folder
+    curr, total = 1, len(listdir("./cogs")) - 1
 
     for filename in listdir("./cogs"):
         if filename.endswith(".py"):
             try:  # load cog
                 bot.reload_extension(f"cogs.{filename[:-3]}")
-                bot._log.info(f"cog {filename} reload, {curr}/{total}")
+                bot.log.info(f"cog {filename} reload, {curr}/{total}")
 
             except Exception as error:  # something in cog wrong
-                bot._log.error(f"error in cog {filename}, {curr}/{total} | {error}")
+                bot.log.error(f"error in cog {filename}, {curr}/{total} | {error}")
 
             curr += 1  # + 1 for current amount
 
 
 def cogsLoad(bot):
-    curr, total = 1, len(listdir("./cogs")) - 3  # cogs - folder
+    curr, total = 1, len(listdir("./cogs")) - 1
 
     for filename in listdir("./cogs"):
         if filename.endswith(".py"):
-            # try: # load cog
-            bot.load_extension(f"cogs.{filename[:-3]}")
-            bot._log.info(f"cog {filename} load, {curr}/{total}")
+            try:  # load cog
+                bot.load_extension(f"cogs.{filename[:-3]}")
+                bot._log.info(f"cog {filename} load, {curr}/{total}")
 
-            # except Exception as error: # something in cog wrong
-            # print(Fore.YELLOW + f"error in cog {filename}, {curr}/{total} | {error}" + Style.RESET_ALL)
-            # logging.error(f"cog filename not load: {error}")
+            except Exception as error:  # something in cog wrong
+                bot.log.error(f"error in cog {filename}, {curr}/{total} | {error}")
 
             curr += 1  # + 1 for current amount
 
 
 def routersLoad(bot):
-    curr, total = 1, len(listdir("./routers")) - 1  # cogs - folder
+    curr, total = 1, len(listdir("./routers")) - 1
 
     for filename in listdir("./routers"):
         if filename.endswith(".py"):
-            # try: # load router
-            bot.load_extension(f"routers.{filename[:-3]}")
-            bot._log.info(f"router {filename} checked, {curr}/{total}")
+            try:  # load router
+                bot.load_extension(f"routers.{filename[:-3]}")
+                bot._log.info(f"router {filename} checked, {curr}/{total}")
 
-            # except Exception as error: # something in cog wrong
-            # print(Fore.YELLOW + f"error in cog {filename}, {curr}/{total} | {error}" + Style.RESET_ALL)
-            # logging.error(f"cog filename not load: {error}")
+            except Exception as error:  # something in cog wrong
+                bot.log.error(f"error in cog {filename}, {curr}/{total} | {error}")
 
             curr += 1  # + 1 for current amount
