e3050745f055f8ba29144df1ef590b64fadd5140
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-05-25 11:31:55 +0300

    autoplay and put_auto

--- src/objects/queue.py
+++ src/objects/queue.py
@@ -19,6 +19,8 @@ class NoirQueue(Queue):
             loose_mode=loose_mode,
         )
 
+        self.api = player.node.rest.ytmclient
+
         self.player = player
 
     # -------------------------------------------------------------------------------------------------------------------------------------
@@ -29,6 +31,17 @@ class NoirQueue(Queue):
 
     # -------------------------------------------------------------------------------------------------------------------------------------
     # Основные функции
+
+    async def put_relayted(self, item: Track):
+        relayted = await self.api.relayted(item)
+
+        if relayted:
+            await self.put_list(relayted)
+
+    def _get(self) -> Track:
+        if self.loose_mode and self.count <= 1:
+            self.player.bot.loop.create_task(self.put_relayted(self._current_item))
+        return super()._get()
 
     async def shuffle(self) -> None:
         super().shuffle()
@@ -42,11 +55,13 @@ class NoirQueue(Queue):
             "put", item.model_dump_json(exclude=["ctx", "requester"])
         )
 
-    async def put_list(self, item: list) -> None:
+    async def put_list(self, item: List[Track]) -> None:
         if self.is_full:
             return
         super().put_list(item)
-        await self.update_state("put_list", len(item))
+        await self.update_state(
+            "put_list", [i.model_dump_json(exclude=["ctx", "requester"]) for i in item]
+        )
 
     async def put_at_index(self, index: int, item: Track) -> None:
         super().put_at_index(index, item)
@@ -67,11 +82,37 @@ class NoirQueue(Queue):
         item: (
             LavalinkTrackLoadingResponse | LavaSearchLoadingResponse | Track | Playlist
         ),
-        put_type: Literal["once", "tracks", "playlists", "mixes"] = "once",
+        put_type: Literal["auto", "once", "tracks", "playlists", "mixes"] = "auto",
     ) -> None:
-        pass
+        if put_type == "auto":
+            if isinstance(item, LavalinkTrackLoadingResponse):
+                if item.loadType == "track":
+                    await self.put(item.data)
+                elif item.loadType == "playlist":
+                    await self.put_list(item.data.tracks)
+                elif item.loadType == "search":
+                    await self.put(item.data[0])
+                else:
+                    return False
+            elif isinstance(item, LavaSearchLoadingResponse):
+                await self.put(item.tracks[0])
+            elif isinstance(item, Playlist, Album):
+                await self.put_list(item.tracks)
+            elif isinstance(item, Track):
+                await self.put(item)
+            else:
+                return False
 
-    async def set_loop_mode(self, mode: LoopMode | None) -> None:
+    async def start_autoplay(self, item: Track) -> None:
+        if item.info.sourceName != "youtube":
+            return
+        if item != self.player.current:
+            await self.player.play(item, noReplace=False)
+        self._loose_mode = True
+        await self.clear()
+        await self.put_relayted(item)
+
+    async def set_loop_mode(self, mode: LoopMode | None = None) -> None:
         if self._queue:
             self._loop_mode = mode
             await self.player.update_controller_once()
