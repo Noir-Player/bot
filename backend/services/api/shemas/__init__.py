610404a7de797fa97cc181645f6091c355cc861f
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2025-07-03 15:51:53 +0300

    remove legacy

--- backend/services/api/shemas/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-## Shemas Module
-import all pydantic models in here
-"""
-
-from api.shemas.main import *
-from api.shemas.playlists import *
-from api.shemas.discovery import *
-from api.shemas.stars import *
--- backend/services/api/shemas/main.py
+++
@@ -1,9 +0,0 @@
-from pydantic import *
-from pydantic.fields import Field
-
-
-class StatusResponse(BaseModel):
-    guilds: int = Field(description= "Количество серверов")
-    players: int = Field(description= "Количество плееров")
-    uptime: int = Field(description= "Аптайм")
-    ping: float = Field(description= "Задержка сервера")
