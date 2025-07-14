ebc12d2aec3a418953dc70d21589411b64ee7647
PersifoX <kolodaznyjvladislav@gmail.com>
2025-07-14 15:12:04 +0300

    entities init now as a docs

--- backend/entities/__init__.py
+++ backend/entities/__init__.py
@@ -1,6 +1,9 @@
 """
-Entities from project
-"""
+## Entities from project
+you can import any entity from this module using `get_instance` function
 
-from .bot import *
-from .config import *
+```python
+from entities.bot import get_instance as get_bot
+bot = get_bot()
+```
+"""
