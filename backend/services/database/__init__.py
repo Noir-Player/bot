3e7bf75fae0110450eb13cced102e3e66f20a101
PersifoX <kolodaznyjvladislav@gmail.com>
2025-07-20 12:29:18 +0300

    add another documents

--- backend/services/database/__init__.py
+++ backend/services/database/__init__.py
@@ -1,7 +1,7 @@
 def get_documents():
     """Get all documents for Beanie"""
     from .models import setup
+    from .models import star
+    from .models import playlist
 
-    return [
-        setup.SetupDocument,
-    ]
+    return [setup.SetupDocument, star.StarDocument, playlist.PlaylistDocument]
