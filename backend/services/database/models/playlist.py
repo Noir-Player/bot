d1a22294ef0b09f1922fccf57aa8d4bc1409a1d5
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2025-07-23 01:02:18 +0300

    some info about playlist

--- backend/services/database/models/playlist.py
+++ backend/services/database/models/playlist.py
@@ -1,16 +1,20 @@
-from services.persiktunes.models import Playlist
-from pydantic import Field
-
 from typing import Annotated
+from uuid import uuid4
 
 from beanie import Document, Indexed
+from pydantic import Field
+from services.persiktunes.models import Playlist
 
 
 class PlaylistDocument(Playlist, Document):
     """Model for Playlist document in Beanie"""
 
-    user_id: Annotated[int, Indexed(unique=True)] = Field(
-        description="ID of the user who owns the playlist"
+    user_id: int = Field(description="ID of the user who owns the playlist")
+
+    public: bool = Field(default=False, description="Is the playlist public?")
+
+    uuid: Annotated[str, Indexed(unique=True)] = Field(
+        default_factory=lambda: uuid4().__str__(), description="UUID of the playlist"
     )
 
     class Settings:
