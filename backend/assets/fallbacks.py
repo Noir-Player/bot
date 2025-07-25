f920db8e784f9b3a9e8ecd4c0338c3d363f51970
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2025-07-26 01:56:44 +0300

    assets

---
+++ backend/assets/fallbacks.py
@@ -0,0 +1,3 @@
+NO_COVER_URL = "https://media.discordapp.net/attachments/1398433650051387523/1398433691046252634/No_cover_image.png?ex=6885585d&is=688406dd&hm=21b0382789d353d8c3987d88956c7f32a4c271eedce9d83bb50dbd8298bddfc7&=&format=webp&quality=lossless"
+
+NO_TRACK_URL = "https://media.discordapp.net/attachments/1398433650051387523/1398434136179605624/No_track_image.png?ex=688558c7&is=68840747&hm=bb259a57838d85af4ac9cb9b8f5e235d97e0b9627b216bf1277c3bb7676e5f76&=&format=webp&quality=lossless"
